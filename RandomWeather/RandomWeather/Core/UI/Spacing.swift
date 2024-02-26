4f5ff99b7452d68dda1242cbf0f86bac0b89fccb
Carles Grau <carles.grau@proton.me>
2024-02-26 20:32:36 +0100

    add view and spacing utils

---
+++ RandomWeather/RandomWeather/Core/UI/Spacing.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+enum Spacing {
+  ///32
+  static let xl: CGFloat = 32
+  ///16
+  static let l: CGFloat = 16
+  ///12
+  static let m: CGFloat = 12
+  ///8
+  static let s: CGFloat = 8
+  ///4
+  static let xs: CGFloat = 4
+}
