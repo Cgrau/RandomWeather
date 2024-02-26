d607e0a45cf198639c8d4d2d78d0283b0993817d
Carles Grau <carles.grau@proton.me>
2024-02-26 19:54:33 +0100

    add gitignore and pods + setup project

---
+++ RandomWeather/RandomWeather/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomWeather
+//
+//  Created by Carles Grau on 26/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      // Do any additional setup after loading the view.
+   }
+
+
+}
+
